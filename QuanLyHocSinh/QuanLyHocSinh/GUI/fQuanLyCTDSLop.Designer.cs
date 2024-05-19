4ef6611f3d048104a2711d3cabd98f64712f8d04
Phương Ngân <169058706+NganPhuong09@users.noreply.github.com>
2024-05-19 22:09:50 +0700

    Add files via upload

---
+++ QuanLyHocSinh/QuanLyHocSinh/GUI/fQuanLyCTDSLop.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace WindowsFormsApp3.GUI
+{
+    partial class fQuanLyCTDSLop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cmbMaHocKi = new System.Windows.Forms.ComboBox();
+            this.cmbMaLop = new System.Windows.Forms.ComboBox();
+            this.cmbMaHocSinh = new System.Windows.Forms.ComboBox();
+            this.txbDTBHocKi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbMaCTDSLop = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvDSLop = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSLop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cmbMaHocKi);
+            this.panel2.Controls.Add(this.cmbMaLop);
+            this.panel2.Controls.Add(this.cmbMaHocSinh);
+            this.panel2.Controls.Add(this.txbDTBHocKi);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnLamMoi);
+            this.panel2.Controls.Add(this.btnXoa);
+            this.panel2.Controls.Add(this.btnSua);
+            this.panel2.Controls.Add(this.btnThem);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txbMaCTDSLop);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(829, 223);
+            this.panel2.TabIndex = 8;
+            // 
+            // cmbMaHocKi
+            // 
+            this.cmbMaHocKi.FormattingEnabled = true;
+            this.cmbMaHocKi.Location = new System.Drawing.Point(677, 31);
+            this.cmbMaHocKi.Name = "cmbMaHocKi";
+            this.cmbMaHocKi.Size = new System.Drawing.Size(106, 33);
+            this.cmbMaHocKi.TabIndex = 2;
+            // 
+            // cmbMaLop
+            // 
+            this.cmbMaLop.FormattingEnabled = true;
+            this.cmbMaLop.Location = new System.Drawing.Point(417, 30);
+            this.cmbMaLop.Name = "cmbMaLop";
+            this.cmbMaLop.Size = new System.Drawing.Size(106, 33);
+            this.cmbMaLop.TabIndex = 1;
+            // 
+            // cmbMaHocSinh
+            // 
+            this.cmbMaHocSinh.FormattingEnabled = true;
+            this.cmbMaHocSinh.Location = new System.Drawing.Point(172, 80);
+            this.cmbMaHocSinh.Name = "cmbMaHocSinh";
+            this.cmbMaHocSinh.Size = new System.Drawing.Size(106, 33);
+            this.cmbMaHocSinh.TabIndex = 3;
+            // 
+            // txbDTBHocKi
+            // 
+            this.txbDTBHocKi.Location = new System.Drawing.Point(445, 85);
+            this.txbDTBHocKi.Margin = new System.Windows.Forms.Padding(6);
+            this.txbDTBHocKi.MaxLength = 10;
+            this.txbDTBHocKi.Name = "txbDTBHocKi";
+            this.txbDTBHocKi.Size = new System.Drawing.Size(108, 33);
+            this.txbDTBHocKi.TabIndex = 4;
+            this.txbDTBHocKi.TextChanged += new System.EventHandler(this.txbDTBHocKi_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "ĐTB học kì";
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.Location = new System.Drawing.Point(567, 151);
+            this.btnLamMoi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(150, 44);
+            this.btnLamMoi.TabIndex = 16;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = true;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(422, 151);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(108, 44);
+            this.btnXoa.TabIndex = 15;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(278, 151);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(108, 44);
+            this.btnSua.TabIndex = 14;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(131, 151);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(117, 44);
+            this.btnThem.TabIndex = 13;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 88);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Mã học sinh";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(567, 37);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Mã học kì";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mã lớp";
+            // 
+            // txbMaCTDSLop
+            // 
+            this.txbMaCTDSLop.Location = new System.Drawing.Point(172, 30);
+            this.txbMaCTDSLop.Margin = new System.Windows.Forms.Padding(6);
+            this.txbMaCTDSLop.MaxLength = 10;
+            this.txbMaCTDSLop.Name = "txbMaCTDSLop";
+            this.txbMaCTDSLop.Size = new System.Drawing.Size(106, 33);
+            this.txbMaCTDSLop.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã CTDS lớp";
+            // 
+            // dgvDSLop
+            // 
+            this.dgvDSLop.AllowUserToAddRows = false;
+            this.dgvDSLop.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dgvDSLop.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDSLop.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvDSLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSLop.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvDSLop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDSLop.Location = new System.Drawing.Point(0, 223);
+            this.dgvDSLop.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDSLop.MultiSelect = false;
+            this.dgvDSLop.Name = "dgvDSLop";
+            this.dgvDSLop.ReadOnly = true;
+            this.dgvDSLop.RowHeadersVisible = false;
+            this.dgvDSLop.RowHeadersWidth = 51;
+            this.dgvDSLop.RowTemplate.Height = 30;
+            this.dgvDSLop.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDSLop.Size = new System.Drawing.Size(829, 253);
+            this.dgvDSLop.TabIndex = 9;
+            this.dgvDSLop.TabStop = false;
+            this.dgvDSLop.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDSLop_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaCTDSLop";
+            this.Column1.HeaderText = "MÃ CTDS LỚP";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 180;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "MaLop";
+            this.Column2.HeaderText = "MÃ LỚP";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 180;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "MaHocKi";
+            this.Column3.HeaderText = "MÃ HỌC KÌ";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 160;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "MaHocSinh";
+            this.Column4.HeaderText = "MÃ HỌC SINH";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 180;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.DataPropertyName = "DTBHocKi";
+            this.Column5.HeaderText = "ĐTB HỌC KÌ";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // fQuanLyCTDSLop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 476);
+            this.Controls.Add(this.dgvDSLop);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "fQuanLyCTDSLop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý CT_DS lớp";
+            this.Load += new System.EventHandler(this.fQuanLyCTDSLop_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSLop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbDTBHocKi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLamMoi;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbMaCTDSLop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvDSLop;
+        private System.Windows.Forms.ComboBox cmbMaHocKi;
+        private System.Windows.Forms.ComboBox cmbMaLop;
+        private System.Windows.Forms.ComboBox cmbMaHocSinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
