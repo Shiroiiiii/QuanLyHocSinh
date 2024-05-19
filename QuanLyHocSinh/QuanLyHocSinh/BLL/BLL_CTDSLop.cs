f453fd0fe8d0a22e6dd8ff2809a810dab213ab8f
Phương Ngân <169058706+NganPhuong09@users.noreply.github.com>
2024-05-19 21:13:10 +0700

    Add files via upload

---
+++ QuanLyHocSinh/QuanLyHocSinh/BLL/BLL_CTDSLop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.BLL
+{
+    public class BLL_CTDSLop
+    {
+        private static BLL_CTDSLop instance;//ctr + r + e
+        public static BLL_CTDSLop Instance
+        {
+            get { if (instance == null) instance = new BLL_CTDSLop(); return instance; }
+            private set => instance = value;
+        }
+        private BLL_CTDSLop() { }
+        public DataTable DanhSach()
+        {
+            return BLL_CTDSLop.Instance.DanhSach();
+        }
+        public bool Them(string mactdslop, string malop, string mahocki, string mahocsinh, decimal dtbhocki)
+        {
+            return BLL_CTDSLop.Instance.Them(mactdslop,malop,mahocki,mahocsinh,dtbhocki);
+        }
+        public bool Sua(string mactdslop, string malop, string mahocki, string mahocsinh, decimal dtbhocki)
+        {
+            return BLL_CTDSLop.Instance.Sua(mactdslop, malop, mahocki, mahocsinh, dtbhocki);
+        }
+        public bool Xoa(string mactdslop, string malop, string mahocki, string mahocsinh, decimal dtbhocki)
+        {
+            return BLL_CTDSLop.Instance.Xoa( mactdslop, malop, mahocki, mahocsinh, dtbhocki);
+        }
+    }
+}
---
+++ QuanLyHocSinh/QuanLyHocSinh/BLL/BLL_Lop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.BLL
+{
+    public class BLL_Lop
+    {
+        private static readonly Lazy<BLL_Lop> lazyInstance = new Lazy<BLL_Lop>(() => new BLL_Lop());
+        public static BLL_Lop Instance
+        {
+            get { return lazyInstance.Value; }
+        }
+
+        /*private static BLL_Lop instance;//ctr + r + e
+        public static BLL_Lop Instance
+        {
+            get { if (instance == null) instance = new BLL_Lop(); return instance; }
+            private set => instance = value;
+        }*/
+        private BLL_Lop() { }
+        public DataTable DanhSach()
+        {
+            return BLL_Lop.Instance.DanhSach();
+        }
+        public bool Them(string malop, string tenlop, int siso, string makhoilop, string manamhoc)
+        {
+            return BLL_Lop.Instance.Them(malop, tenlop, siso,makhoilop, manamhoc);
+        }
+        public bool Sua(string malop, string tenlop, int siso, string makhoilop, string manamhoc)
+        {
+            return BLL_Lop.Instance.Sua(malop, tenlop, siso, makhoilop, manamhoc);
+        }
+        public bool Xoa(string malop, string tenlop, int siso, string makhoilop, string manamhoc)
+        {
+            return BLL_Lop.Instance.Xoa(malop, tenlop, siso, makhoilop, manamhoc);
+        }
+    }
+}
