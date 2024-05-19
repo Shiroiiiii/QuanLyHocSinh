eb71e9be5eed2e01854dabfe0c783dad2e55a208
Phương Ngân <169058706+NganPhuong09@users.noreply.github.com>
2024-05-19 21:15:42 +0700

    Add files via upload

---
+++ QuanLyHocSinh/QuanLyHocSinh/DAL/DAL_MonHoc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.DAL
+{
+    public class DAL_MonHoc
+    {
+        private static DAL_MonHoc instance;//ctr + r + e
+        public static DAL_MonHoc Instance
+        {
+            get { if (instance == null) instance = new DAL_MonHoc(); return instance; }
+            private set => instance = value;
+        }
+        private DAL_MonHoc() { }
+        public bool Them(string mamonhoc, string tenmonhoc, int heso)
+        {
+            string sql = "insert into MonHoc (MaMonHoc,TenMonHoc,HeSo) values(@MaMonHoc,@TenMonHoc,@HeSo)";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] {mamonhoc, tenmonhoc, heso});
+        }
+        public bool Sua(string mamonhoc, string tenmonhoc, int heso)
+        {
+            string sql = "update MonHoc set MaMonHoc = @MaMonHoc, TenMonHoc = @TenMonHoc, HeSo=@HeSo ";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] { mamonhoc, tenmonhoc, heso });
+        }
+        public bool Xoa(string mamonhoc, string tenmonhoc, int heso)
+        {
+            string sql = "delete from MonHoc where MaMonHoc = @MaMonHoc, TenMonHoc = @TenMonHoc, HeSo=@HeSo";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] { mamonhoc, tenmonhoc, heso });
+        }
+        public DataTable DanhSach()
+        {
+            return KetNoi.Instance.ExcuteQuery("select * from MonHoc");
+        }
+    }
+}
---
+++ QuanLyHocSinh/QuanLyHocSinh/DAL/DAL_NamHoc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.DAL
+{
+    public class DAL_NamHoc
+    {
+        private static DAL_NamHoc instance;//ctr + r + e
+        public static DAL_NamHoc Instance
+        {
+            get { if (instance == null) instance = new DAL_NamHoc(); return instance; }
+            private set => instance = value;
+        }
+        private DAL_NamHoc() { }
+        public bool Them(string manamhoc, string tennamhoc)
+        {
+            string sql = "insert into NamHoc(MaNamHoc, TenNamHoc) values(@MaNamHoc, @TenNamHoc)";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] { manamhoc, tennamhoc });
+        }
+        public bool Sua(string manamhoc, string tennamhoc)
+        {
+            string sql = "update NamHoc set MaNamHoc = @MaNamHoc, TenNamHoc = @TenNamHoc";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] { manamhoc, tennamhoc });
+        }
+        public bool Xoa(string manamhoc, string tennamhoc)
+        {
+            string sql = "delete from NamHoc where MaNamHoc = @MaNamHoc, TenNamHoc = @TenNamHoc";
+            return KetNoi.Instance.ExcuteNonQuery(sql, new object[] { manamhoc, tennamhoc });
+        }
+        public DataTable DanhSach()
+        {
+            return KetNoi.Instance.ExcuteQuery("select * from NamHoc");
+        }
+    }
+}
