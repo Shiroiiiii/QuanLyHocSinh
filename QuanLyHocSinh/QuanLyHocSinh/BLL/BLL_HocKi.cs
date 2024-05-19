795308b55cbc55176ac6dc08393670b43f8381b9
Phương Ngân <169058706+NganPhuong09@users.noreply.github.com>
2024-05-19 20:55:55 +0700

    Add files via upload

---
+++ QuanLyHocSinh/QuanLyHocSinh/BLL/BLL_HocKi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.BLL
+{
+    public class BLL_HocKi
+    {
+        private static BLL_HocKi instance;//ctr + r + e
+        public static BLL_HocKi Instance
+        {
+            get { if (instance == null) instance = new BLL_HocKi(); return instance; }
+            private set => instance = value;
+        }
+        private BLL_HocKi() { }
+        public DataTable DanhSach()
+        {
+            return BLL_HocKi.Instance.DanhSach();
+        }
+        public bool Them(string mahocki, string tenhocki)
+        {
+            return BLL_HocKi.Instance.Them(mahocki,tenhocki);
+        }
+        public bool Sua(string mahocki, string tenhocki)
+        {
+            return BLL_HocKi.Instance.Sua(mahocki, tenhocki);
+        }
+        public bool Xoa(string mahocki, string tenhocki)
+        {
+            return BLL_HocKi.Instance.Xoa(mahocki, tenhocki);
+        }
+    }
+}
---
+++ QuanLyHocSinh/QuanLyHocSinh/BLL/BLL_MonHoc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.BLL
+{
+    public class BLL_MonHoc
+    {
+        private static BLL_MonHoc instance;//ctr + r + e
+        public static BLL_MonHoc Instance
+        {
+            get { if (instance == null) instance = new BLL_MonHoc(); return instance; }
+            private set => instance = value;
+        }
+        private BLL_MonHoc() { }
+        public DataTable DanhSach()
+        {
+            return BLL_MonHoc.Instance.DanhSach();
+        }
+        public bool Them(string mamonhoc, string tenmonhoc,int heso)
+        {
+            return BLL_MonHoc.Instance.Them(mamonhoc, tenmonhoc,heso);
+        }
+        public bool Sua(string mamonhoc, string tenmonhoc, int heso)
+        {
+            return BLL_MonHoc.Instance.Sua(mamonhoc, tenmonhoc, heso);
+        }
+        public bool Xoa(string mamonhoc, string tenmonhoc, int heso)
+        {
+            return BLL_MonHoc.Instance.Xoa(mamonhoc, tenmonhoc, heso);
+        }
+    }
+}
